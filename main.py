df6856eea522404c4c1354d658b24f9eafe4e2aa
JBolze <j.bolze94@gmail.com>
2025-01-22 23:19:48 -0300

    Got the player triangle to show up on screen hooray

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    while pygame.get_init():
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+    
+        screen.fill("black")
+        player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
